a1e7f812a52b27df30cb5e5fbd27d62411c29dc9
Scott Pullen <s.pullen05@gmail.com>
2016-01-12 20:27:52 -0500

    adding a list for past searches

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,9 +1,13 @@
 class SearchResultsController < ApplicationController
 
+  def index
+    @search_results = SearchResult.order(created_at: :desc)
+  end
+
   def show
     search_result = SearchResult.find(params[:id])
     file_path = "#{Rails.root}/public#{search_result.document.url(:original, false)}"
-    send_file(file_path, type: 'text/csv; charset=iso-8859-1; header=present', disposition: "attachment; filename=#{search_result.search_uuid}.csv")
+    send_file(file_path, type: 'text/csv; charset=iso-8859-1; header=present', disposition: "attachment; filename=#{search_result.search}_#{search_result.search_uuid}.csv")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resource :search, only: [:show, :create]
-  resources :search_results, only: [:show]
+  resources :search_results, only: [:index, :show]
 
   root to: 'searches#show'
 
